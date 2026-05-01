f4fa40fe7354f6f4c89bca7fbf86f26cc0e1e46a
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-01 02:32:40 -0700

    Align Android KMP target

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,10 +90,14 @@ kotlin {
 }
 
 kotlin {
-    androidLibrary {
+    android {
         namespace = "io.github.kotlinmania.cansi"
         compileSdk = 34
         minSdk = 24
+        withHostTestBuilder {}.configure {}
+        withDeviceTestBuilder {
+            sourceSetTreeName = "test"
+        }
     }
 }
 
