b6bee5d00bed906504efa989360487868d187122
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-23 18:57:30 -0700

    Wire Kotlin → Swift Export → swift test loop into CI (#12)

--- build.gradle.kts
+++ build.gradle.kts
@@ -209,10 +209,18 @@ kotlin {
         binaries.framework { baseName = "Cansi"; xcf.add(this) }
     }
     iosSimulatorArm64 {
-        binaries.framework { baseName = "Cansi"; xcf.add(this) }
+        binaries.framework {
+            baseName = "Cansi"
+            isStatic = true
+            xcf.add(this)
+        }
     }
     iosX64 {
-        binaries.framework { baseName = "Cansi"; xcf.add(this) }
+        binaries.framework {
+            baseName = "Cansi"
+            isStatic = true
+            xcf.add(this)
+        }
     }
 
     tvosArm64 {
